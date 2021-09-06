44b9215e8bd4b4ba0ffdcdc244abb20944a80e4a
Davi Otavio <85647007+daviotavio@users.noreply.github.com>
2021-09-06 11:28:49 -0300

    Add files via upload

---
+++ altura alto ou baixo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace altura_alto_ou_baixo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int altura;
+
+            Console.Write("Qual a sua altura em centímetros? ");
+            altura = int.Parse(Console.ReadLine()); 
+
+            if (altura >= 180)
+            {
+                Console.Write("Alto");
+            }
+            else
+            {
+                Console.WriteLine("Baixo");
+            }
+        }
+
+    }
+}
